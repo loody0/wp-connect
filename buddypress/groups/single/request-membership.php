b744d19f9321fb6e314d7fc53bde2f2585d2a013
mbkom <mbkom@mbkom-PC>
2015-09-21 23:02:17 +0800

    requests loop

--- buddypress/groups/single/request-membership.php
+++ buddypress/groups/single/request-membership.php
@@ -1,39 +1,41 @@
 <?php
-
 /**
  * Fires before the display of the group membership request form.
  *
  * @since BuddyPress (1.1.0)
  */
-do_action( 'bp_before_group_request_membership_content' ); ?>
+do_action( 'bp_before_group_request_membership_content' );
+?>
 
 <?php if ( !bp_group_has_requested_membership() ) : ?>
-	<p><?php printf( __( "You are requesting to become a member of the group '%s'.", "buddypress" ), bp_get_group_name( false ) ); ?></p>
+    <p><?php printf( __( "You are requesting to become a member of the group '%s'.", "buddypress" ), bp_get_group_name( false ) ); ?></p>
 
-	<form action="<?php bp_group_form_action('request-membership' ); ?>" method="post" name="request-membership-form" id="request-membership-form" class="standard-form">
-		<label for="group-request-membership-comments"><?php _e( 'Comments (optional)', 'buddypress' ); ?></label>
-		<textarea name="group-request-membership-comments" id="group-request-membership-comments"></textarea>
+    <form action="<?php bp_group_form_action( 'request-membership' ); ?>" method="post" name="request-membership-form" id="request-membership-form" class="standard-form">
+        <div class="form-group">
+            <label for="group-request-membership-comments"><?php _e( 'Comments (optional)', 'buddypress' ); ?></label>
+            <textarea name="group-request-membership-comments" id="group-request-membership-comments" class="form-control"></textarea>
+        </div>
 
-		<?php
+        <?php
+        /**
+         * Fires after the textarea for the group membership request form.
+         *
+         * @since BuddyPress (1.1.0)
+         */
+        do_action( 'bp_group_request_membership_content' );
+        ?>
 
-		/**
-		 * Fires after the textarea for the group membership request form.
-		 *
-		 * @since BuddyPress (1.1.0)
-		 */
-		do_action( 'bp_group_request_membership_content' ); ?>
+        <p><input type="submit" class="btn btn-primary connect-btn connect-btn-primary" name="group-request-send" id="group-request-send" value="<?php esc_attr_e( 'Send Request', 'buddypress' ); ?>" />
 
-		<p><input type="submit" name="group-request-send" id="group-request-send" value="<?php esc_attr_e( 'Send Request', 'buddypress' ); ?>" />
-
-		<?php wp_nonce_field( 'groups_request_membership' ); ?>
-	</form><!-- #request-membership-form -->
+            <?php wp_nonce_field( 'groups_request_membership' ); ?>
+    </form><!-- #request-membership-form -->
 <?php endif; ?>
 
 <?php
-
 /**
  * Fires after the display of the group membership request form.
  *
  * @since BuddyPress (1.1.0)
  */
-do_action( 'bp_after_group_request_membership_content' ); ?>
+do_action( 'bp_after_group_request_membership_content' );
+?>
--- buddypress/groups/single/requests-loop.php
+++ buddypress/groups/single/requests-loop.php
@@ -1,79 +1,88 @@
 <?php if ( bp_group_has_membership_requests( bp_ajax_querystring( 'membership_requests' ) ) ) : ?>
 
-	<div id="pag-top" class="pagination">
+    <div id="pag-top" class="pagination connect-pagination connect-pagination-top clearfix">
 
-		<div class="pag-count" id="group-mem-requests-count-top">
+        <div class="pag-count pull-left" id="group-mem-requests-count-top">
 
-			<?php bp_group_requests_pagination_count(); ?>
+            <?php bp_group_requests_pagination_count(); ?>
 
-		</div>
+        </div>
 
-		<div class="pagination-links" id="group-mem-requests-pag-top">
+        <div class="pagination-links pull-right" id="group-mem-requests-pag-top">
 
-			<?php bp_group_requests_pagination_links(); ?>
+            <?php bp_group_requests_pagination_links(); ?>
 
-		</div>
+        </div>
 
-	</div>
+    </div>
+    <div class="members dir-list">
 
-	<ul id="request-list" class="item-list">
-		<?php while ( bp_group_membership_requests() ) : bp_group_the_membership_request(); ?>
+        <ul id="request-list" class="item-list">
+            <?php while ( bp_group_membership_requests() ) : bp_group_the_membership_request(); ?>
 
-			<li>
-				<?php bp_group_request_user_avatar_thumb(); ?>
-				<h4><?php bp_group_request_user_link(); ?> <span class="comments"><?php bp_group_request_comment(); ?></span></h4>
-				<span class="activity"><?php bp_group_request_time_since_requested(); ?></span>
+                <li>
+                    <div class="item-avatar">
+                        <?php bp_group_request_user_avatar_thumb(); ?>
+                    </div>
+                    <div class="item">
+                        <div class="item-name">
+                            <?php bp_group_request_user_link(); ?> 
+                        </div>
+                        <span class="activity"><?php bp_group_request_time_since_requested(); ?></span>
+                        <div class="item-content comments"><?php bp_group_request_comment(); ?></div>
 
-				<?php
+                        <?php
+                        /**
+                         * Fires inside the groups membership request list loop.
+                         *
+                         * @since BuddyPress (1.1.0)
+                         */
+                        do_action( 'bp_group_membership_requests_admin_item' );
+                        ?>
+                    </div>
 
-				/**
-				 * Fires inside the groups membership request list loop.
-				 *
-				 * @since BuddyPress (1.1.0)
-				 */
-				do_action( 'bp_group_membership_requests_admin_item' ); ?>
+                    <div class="action">
 
-				<div class="action">
+                        <?php bp_button( array( 'wrapper' =>'span', 'id' => 'group_membership_accept', 'component' => 'groups', 'wrapper_class' => 'accept', 'link_href' => bp_get_group_request_accept_link(), 'link_title' => __( 'Accept', 'buddypress' ), 'link_text' => __( 'Accept', 'buddypress' ), 'link_class' => ' btn btn-primary connect-btn connect-btn-primary' ) ); ?>
 
-					<?php bp_button( array( 'id' => 'group_membership_accept', 'component' => 'groups', 'wrapper_class' => 'accept', 'link_href' => bp_get_group_request_accept_link(), 'link_title' => __( 'Accept', 'buddypress' ), 'link_text' => __( 'Accept', 'buddypress' ) ) ); ?>
+                        <?php bp_button( array( 'wrapper' =>'span', 'id' => 'group_membership_reject', 'component' => 'groups', 'wrapper_class' => 'reject', 'link_href' => bp_get_group_request_reject_link(), 'link_title' => __( 'Reject', 'buddypress' ), 'link_text' => __( 'Reject', 'buddypress' ), 'link_class' => ' btn btn-default connect-btn connect-btn-default' ) ); ?>
 
-					<?php bp_button( array( 'id' => 'group_membership_reject', 'component' => 'groups', 'wrapper_class' => 'reject', 'link_href' => bp_get_group_request_reject_link(), 'link_title' => __( 'Reject', 'buddypress' ), 'link_text' => __( 'Reject', 'buddypress' ) ) ); ?>
+                        <?php
+                        /**
+                         * Fires inside the list of membership request actions.
+                         *
+                         * @since BuddyPress (1.1.0)
+                         */
+                        do_action( 'bp_group_membership_requests_admin_item_action' );
+                        ?>
 
-					<?php
+                    </div>
+                </li>
 
-					/**
-					 * Fires inside the list of membership request actions.
-					 *
-					 * @since BuddyPress (1.1.0)
-					 */
-					do_action( 'bp_group_membership_requests_admin_item_action' ); ?>
+            <?php endwhile; ?>
+        </ul>
+    </div>
 
-				</div>
-			</li>
+    <div id="pag-bottom" class="pagination connect-pagination clearfix">
 
-		<?php endwhile; ?>
-	</ul>
+        <div class="pag-count pull-left" id="group-mem-requests-count-bottom">
 
-	<div id="pag-bottom" class="pagination">
+            <?php bp_group_requests_pagination_count(); ?>
 
-		<div class="pag-count" id="group-mem-requests-count-bottom">
+        </div>
 
-			<?php bp_group_requests_pagination_count(); ?>
+        <div class="pagination-links pull-right" id="group-mem-requests-pag-bottom">
 
-		</div>
+            <?php bp_group_requests_pagination_links(); ?>
 
-		<div class="pagination-links" id="group-mem-requests-pag-bottom">
+        </div>
 
-			<?php bp_group_requests_pagination_links(); ?>
+    </div>
 
-		</div>
+<?php else: ?>
 
-	</div>
+    <div id="message" class="info">
+        <p><?php _e( 'There are no pending membership requests.', 'buddypress' ); ?></p>
+    </div>
 
-	<?php else: ?>
-
-		<div id="message" class="info">
-			<p><?php _e( 'There are no pending membership requests.', 'buddypress' ); ?></p>
-		</div>
-
-	<?php endif; ?>
+<?php endif; ?>
